7bd200a5b3372eb1b2b8dff9e14415c6bf30ae01
simon <simon.hora@post.cz>
2026-05-18 19:22:43 +0200

    Added AudioTrack.java

--- src/logic/AudioTrack.java
+++ src/logic/AudioTrack.java
@@ -1,15 +1,48 @@
 package logic;
 
-import java.util.ArrayList;
-import java.util.List;
+public class AudioTrack implements Playable {
+    private String instrumentName;
+    private int midiNote;
+    private boolean[] beats = new boolean[16];
 
-public abstract class AudioTrack implements Playable {
-    protected String instrumentName;
-    protected int volume;
-
-    protected List<Integer> notes = new ArrayList<>();
-
-    public AudioTrack(String name){
+    public AudioTrack(String name, int midiNote) {
         this.instrumentName = name;
+        this.midiNote = midiNote;
+    }
+
+    public String getInstrumentName() {
+        return instrumentName;
+    }
+
+    public int getMidiNote() {
+        return midiNote;
+    }
+
+    public boolean[] getBeats() {
+        return beats;
+    }
+
+    public void setBeat(int index, boolean active) {
+        if (index >= 0 && index < 16) {
+            beats[index] = active;
+        }
+    }
+
+    public boolean isBeatActive(int index) {
+        if (index >= 0 && index < 16) {
+            return beats[index];
+        }
+        return false;
+    }
+
+    @Override
+    public void play() {
+
+        System.out.println("Playing track: " + instrumentName);
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping track: " + instrumentName);
     }
 }
